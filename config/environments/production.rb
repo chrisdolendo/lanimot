07071511a622f6f0b280005c5e4558f072bb1646
Christine Dolendo <chris.dolendo@gmail.com>
2014-10-12 17:44:56 -0700

    compile assets line on production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,10 @@ Lanimot::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.serve_static_assets = true
+  config.assets.compile = true
+
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
